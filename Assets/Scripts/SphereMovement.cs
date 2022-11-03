00304b334c9ea83c1999a8005cc55e4d147a530e
mineas312 <kaybenot@gmail.com>
2022-11-03 19:19:11 +0100

    Block walljumps

--- Assets/Scripts/SphereMovement.cs
+++ Assets/Scripts/SphereMovement.cs
@@ -8,12 +8,14 @@ public class SphereMovement : MonoBehaviour
 {
     [SerializeField, Range(0f, 100f)] private float maxSpeed = 10f;
     [SerializeField, Range(0f, 100f)] private float maxAcceleration = 10f;
-    [SerializeField, Range(0f, 10f)] private float jumpHeight = 5f;
+    [SerializeField, Range(0f, 10f)] private float jumpHeight = 2f;
+    [SerializeField, Range(0, 5)] private int maxAirJumps = 0;
 
     private Vector3 velocity;
     private Vector3 acceleration;
     private Rigidbody body;
     private bool onGround;
+    private int jumpPhase = 0;
 
     private void Awake()
     {
@@ -22,27 +24,45 @@ public class SphereMovement : MonoBehaviour
 
     void FixedUpdate()
     {
+        updateState();
         onGround = false;
-        velocity = body.velocity;
         float maxSpeedChange = maxAcceleration * Time.fixedDeltaTime;
         velocity.x = Mathf.MoveTowards(velocity.x, acceleration.x, maxSpeedChange);
         velocity.z = Mathf.MoveTowards(velocity.z, acceleration.z, maxSpeedChange);
         body.velocity = velocity;
     }
 
-    void OnCollisionStay(Collision other)
+    private void updateState()
     {
-        onGround = true;
+        velocity = body.velocity;
+        if (onGround)
+            jumpPhase = 0;
     }
 
-    private void EvaluateCollision(Collision collision)
+    void OnCollisionEnter(Collision other)
     {
+        evaluateCollision(other);
+    }
+
+    void OnCollisionStay(Collision other)
+    {
+        evaluateCollision(other);
+    }
+
+    private void evaluateCollision(Collision collision)
+    {
+        for (int i = 0; i < collision.contactCount; i++)
+        {
+            Vector3 normal = collision.GetContact(i).normal;
+            onGround |= normal.y >= 0.9f;
+        }
     }
 
     public void Jump(InputAction.CallbackContext context)
     {
-        if (context.performed && onGround)
+        if (context.performed && (onGround || jumpPhase < maxAirJumps))
         {
+            jumpPhase += 1;
             var jumpForce = Mathf.Sqrt(-2f * Physics.gravity.y * jumpHeight);
             body.velocity = body.velocity += new Vector3(0f, jumpForce, 0f);
         }
